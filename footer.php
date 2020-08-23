fdee35c7357c60fec4ec8b5f827646c259aaed69
workssnk823 <work.ssnk.0823@gmail.com>
2020-08-23 18:13:32 +0900

    DB修正

--- footer.php
+++ footer.php
@@ -2,53 +2,41 @@
   <div class="container">
     <div class="row">
       <div class="f_info col-xs-12 col-sm-4 col-md-4 col-lg-4">
-        <div class="logo"><img src="http://www.kurokitec.com/common/img/footer/f_logo.png" srcset="http://www.kurokitec.com/common/img/footer/f_logo.png 1x,http://www.kurokitec.com/common/img/footer/f_logo@2x.png 2x" alt="黒木テック工業株式会社"></div>
-        <p class="add">黒木テック工業株式会社<br>
-          〒664‑0027 兵庫県伊丹市池尻3丁目302番地<br>
-          tel 072-781-1950 / fax 072-779-7070</p>
+        <div class="logo"><img src="<?php echo get_template_directory_uri(); ?>/images/logoSample.jpg" alt="株式会社CLIMATESTAR'S"></div>
+        <p class="add">株式会社 クライメイトスターズ/<br>地域活性化団体川西風土/Gotuo<br><br>代表取締役 斉藤雄大<br></p>
           <div class="common_link"><a href="http://www.kurokitec.com/contact/"><span>お問い合わせ</span></a></div>
           <ul class="snsList">
             <li><a href="https://www.facebook.com/kurokitec/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+            <li><a href="https://www.twitter.com/kurokitec/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
             <li><a href="https://www.instagram.com/kurokitec/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
           </ul>
-          <p class="copy">© KUROKI TEC Corporation.</p>
+          <p class="copy">© CLIMATESTAR'S Corporation.</p>
           <!-- /.f_info --></div>
           <div class="f_menu col-xs-12 col-sm-8 col-md-8 col-lg-8 hidden-xs">
-            <div class="bnr_area"> <a href="http://www.kurokitec.com/column/"><span class="img"><img src="http://www.kurokitec.com/common/img/footer/f_bnr_ico.png" srcset="http://www.kurokitec.com/common/img/footer/f_bnr_ico.png 1x,http://www.kurokitec.com/common/img/footer/f_bnr_ico@2x.png 2x" alt="スタッフブログ"></span>
-              <p class="txt">遊園地の乗り物屋さん日誌</p>
-              <span class="arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></a> </div>
               <div class="f_menu_list">
-                <ul class="list">
-                  <li><span>クロキテックを知る</span>
-                    <ul>
-                      <li><a href="http://www.kurokitec.com/about/thought/">ものづくりへの想い</a></li>
-                      <li><a href="http://www.kurokitec.com/about/threemin/">3分でわかるクロキテック</a></li>
-                      <li><a href="http://www.kurokitec.com/about/history/">クロキテックの歩み</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="http://www.kurokitec.com/factory/">工場案内</a></li>
-                  <li><a href="http://www.kurokitec.com/flow/">ご依頼から納品までの流れ</a></li>
-                </ul>
-                <ul class="list">
-                  <li><span>サービス案内</span>
-                    <ul>
-                      <li><a href="http://www.kurokitec.com/service/business/">事業内容</a></li>
-                      <li><a href="http://www.kurokitec.com/works/">製作実績</a></li>
-                      <li><a href="http://www.kurokitec.com/service/advantage/">クロキテックの強み</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="http://www.kurokitec.com/recruit/">採用情報</a></li>
-                  <li><a href="http://www.kurokitec.com/info/">お知らせ</a></li>
-                </ul>
                 <ul class="list">
                   <li><span>会社概要</span>
                     <ul>
-                      <li><a href="http://www.kurokitec.com/company/message/">代表メッセージ</a></li>
-                      <li><a href="http://www.kurokitec.com/company/profile/">会社案内</a></li>
-                      <li><a href="http://www.kurokitec.com/company/concept/">遊びココロで地域活性</a></li>
+                      <li><a href="http://www.kurokitec.com/about/thought/">代表メッセージ</a></li>
+                      <li><a href="http://www.kurokitec.com/about/threemin/">活動方針</a></li>
+                      <li><a href="http://www.kurokitec.com/about/threemin/">活動内容・コンセプト</a></li>
+                      <li><a href="http://www.kurokitec.com/about/threemin/">主要取引先</a></li>
                     </ul>
                   </li>
-                  <li><a href="http://www.kurokitec.com/partner/">特注製作のご相談</a></li>
+                </ul>
+                <ul class="list">
+                  <li><span>事業内容</span>
+                    <ul>
+                      <li><a href="http://www.kurokitec.com/service/business/">地域活性化事業</a></li>
+                      <li><a href="http://www.kurokitec.com/works/">イベント事業</a></li>
+                      <li><a href="http://www.kurokitec.com/service/advantage/">地方ブランディング総合事業</a></li>
+                      <li><a href="http://www.kurokitec.com/works/">物流事業</a></li>
+                      <li><a href="http://www.kurokitec.com/works/">総合デザインサービス事業</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                <ul class="list">
+                  <li><span>Blog</span></li>
                   <li><a href="http://www.kurokitec.com/contact/">お問い合わせ</a></li>
                 </ul>
                 <!-- /.f_menu_list --></div>
--- index.php
+++ index.php
@@ -6,8 +6,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/headerStyle.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/footerStyle.css">
+<link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
+
 <!--todo文字種変更-->
-<link href="https://fonts.googleapis.com/css?family=Permanent+Marker rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/header.js"></script>
