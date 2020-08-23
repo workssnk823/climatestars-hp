93f82b2a03b886e524a2b91b1e0ee038f0070859
workssnk823 <suke.badminton7@icloud.com>
2020-08-23 14:14:16 +0900

    ヘッダーフッターテンプレート追加

---
+++ footer.php
@@ -0,0 +1,6 @@
+
+<footer>
+<div class="container">
+<p class="text-center">Copyright © Webst8 All Rights Reserved.</p>
+</div>
+</footer>
---
+++ header.php
@@ -0,0 +1,24 @@
+
+<header>
+<nav class="navbar navbar-default" style="margin-bottom:0;">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-navbar-collapse-1">
+      <span class="sr-only">メニュー</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+  </div>
+  <div id="bs-navbar-collapse-1" class="collapse navbar-collapse">
+    <ul class="nav navbar-nav">
+      <li><a href="#">HOME</a></li>
+      <li><a href="#">PROFILE</a></li>
+      <li><a href="#">BLOG</a></li>
+      <li><a href="#">CONTACT</a></li>
+    </ul>
+  </div>
+</nav>
+<div id="top-vg">
+<img src="<?php echo get_template_directory_uri(); ?>/images/top.jpeg" alt="トップページ画像" >
+</div>
+</header>
